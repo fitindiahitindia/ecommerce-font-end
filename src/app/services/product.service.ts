290b12a0b5a906d81abf5f1f5568bf2ed2d5cb44
birender <birenderk935@gmail.com>
2024-06-12 17:28:16 +0530

    build 1.0

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -27,7 +27,7 @@ export class ProductService {
   // URL = "http://localhost:5000/api/v1";
   // URL="https://ecommerce-api-topaz.vercel.app/api/v1"
      URL="https://ecommerce-api-weld.vercel.app/api/v1"
-
+  
   get_product():Observable<any>{
    const adminlogintoken=this.getAdminLoginToken();
    const headers = {"Authorization":"Bearer "+adminlogintoken}
